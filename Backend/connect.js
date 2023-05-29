f991df853ec5f1210918343ca6f298f9b5830f98
Antoni Garczyński <antek.garczynski@gmail.com>
2023-05-29 23:41:13 +0200

    Finalna wersja strony

--- Backend/connect.js
+++ Backend/connect.js
@@ -3,6 +3,6 @@ import mysql from 'mysql2';
 export const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '12345',
-  database: 'bank'
+  password: '',
+  database: 'bank',
 });
